cefc38795d7f1433e4eca342ce57cb12a8596b27
brownguo <yexuan.guo@codedance.cn>
2019-02-11 15:36:58 +0800

    a timeline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 //import Button from 'antd/lib/button';
 import { Button } from 'antd';
+import { Timeline,Icon } from 'antd';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,14 @@ class App extends Component {
         return (
             <div className="App">
                 <Button type="primary">Halo world!</Button>
+                <Timeline mode="alternate">
+                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                    <Timeline.Item color="green">Solve initial network problems 2015-09-01</Timeline.Item>
+                    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>Hello World!</Timeline.Item>
+                    <Timeline.Item color="red">Network problems being solved 2015-09-01</Timeline.Item>
+                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>Technical testing 2015-09-01</Timeline.Item>
+                </Timeline>
             </div>
         );
     }
