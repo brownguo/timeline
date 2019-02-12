739a84771517d88c181998f71a8a5a05a96fd17a
brownguo <yexuan.guo@codedance.cn>
2019-02-12 19:51:39 +0800

    list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb,Timeline,List,Icon,Avatar} from 'antd';
 
 const { Header, Content, Footer } = Layout;
+const listData = [];
+for (let i = 0; i < 23; i++) {
+    listData.push({
+        href: 'http://ant.design',
+        title: `ant design part ${i}`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+}
+
+const IconText = ({ type, text }) => (
+    <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+        {text}
+  </span>
+);
 class App extends Component {
     render() {
         return (
@@ -27,7 +44,48 @@ class App extends Component {
                             <Breadcrumb.Item>List</Breadcrumb.Item>
                             <Breadcrumb.Item>App</Breadcrumb.Item>
                         </Breadcrumb>
-                        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+                        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                            <Timeline>
+                                <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+                                <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+                                <Timeline.Item color="red">
+                                    <p>Solve initial network problems 1</p>
+                                    <p>Solve initial network problems 2</p>
+                                    <p>Solve initial network problems 3 2015-09-01</p>
+                                </Timeline.Item>
+                                <Timeline.Item>
+                                    <p>Technical testing 1</p>
+                                    <p>Technical testing 2</p>
+                                    <p>Technical testing 3 2015-09-01</p>
+                                </Timeline.Item>
+                            </Timeline>
+                            <List
+                                itemLayout="vertical"
+                                size="large"
+                                pagination={{
+                                    onChange: (page) => {
+                                        console.log(page);
+                                    },
+                                    pageSize: 3,
+                                }}
+                                dataSource={listData}
+                                footer={<div><b>ant design</b> footer part</div>}
+                                renderItem={item => (
+                                    <List.Item
+                                        key={item.title}
+                                        actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+                                        extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+                                    >
+                                        <List.Item.Meta
+                                            avatar={<Avatar src={item.avatar} />}
+                                            title={<a href={item.href}>{item.title}</a>}
+                                            description={item.description}
+                                        />
+                                        {item.content}
+                                    </List.Item>
+                                )}
+                            />
+                        </div>
                     </Content>
                     <Footer style={{ textAlign: 'center' }}>
                         Ant Design ©2018 Created by Ant UED
